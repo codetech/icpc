ea5c064f7d39c23ace937d779d04fcb5e2224d43
Jackson Hamilton <jackson@jacksonrayhamilton.com>
2015-11-13 22:07:24 -0800

    Trim and improve Emacs configuration.

--- configs/init.el
+++ configs/init.el
@@ -19,6 +19,11 @@
 
 ;;; Keybindings
 
+(defun ct-alternate-buffer ()
+  "Alternate between the current buffer and the previous."
+  (interactive)
+  (switch-to-buffer (caar (window-prev-buffers))))
+
 ;; Use more ergonomic keybindings.
 
 (cua-mode)
@@ -26,6 +31,7 @@
 (global-set-key (kbd "C-a")   'mark-whole-buffer)
 (global-set-key (kbd "C-o")   'find-file)
 (global-set-key (kbd "C-b")   'switch-to-buffer)
+(global-set-key (kbd "C-p")   'ct-alternate-buffer)
 (global-set-key (kbd "C-s")   'save-buffer)
 (global-set-key (kbd "C-M-r") 'revert-buffer)
 (global-set-key (kbd "C-w")   'kill-this-buffer)
@@ -39,15 +45,17 @@
 (global-set-key (kbd "C-2")   'split-window-below)
 (global-set-key (kbd "C-3")   'split-window-right)
 (global-set-key (kbd "C-7")   'eval-last-sexp)
+(global-set-key (kbd "C-8")   'eval-buffer)
+(global-set-key (kbd "C-9")   'edebug-defun)
 
 (define-key dired-mode-map   (kbd "C-o") 'find-file)
 (define-key isearch-mode-map (kbd "C-v") 'isearch-yank-kill)
 (define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
 
-(defun codetech-ido-setup-hook ()
+(defun ct-ido-setup-hook ()
   (define-key ido-completion-map (kbd "C-o") 'ido-fallback-command))
 
-(add-hook 'ido-setup-hook 'codetech-ido-setup-hook)
+(add-hook 'ido-setup-hook 'ct-ido-setup-hook)
 
 ;;; Appearance
 
@@ -61,16 +69,15 @@
 (show-paren-mode)
 (setq show-paren-delay 0)
 
+(load-theme 'adwaita)
+
 ;;; Formatting
 
-(setq-default indent-tabs-mode nil
-              require-final-newline t)
+(setq-default indent-tabs-mode nil)
 
 ;;; Features
 
-(electric-indent-mode)
 (electric-pair-mode)
-(setq electric-pair-skip-self nil)
 (global-subword-mode)
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 
@@ -95,63 +102,29 @@
 
 ;;; Compilation
 
-(defun codetech-recompile ()
-  "Recompile and restore window state."
-  (interactive)
-  (let ((window-count (count-windows))
-        cleanup maybe-cleanup)
-    (setq cleanup
-          (lambda (buffer)
-            (with-current-buffer buffer
-              (cond
-               ((> window-count 1) (kill-buffer))
-               (t (kill-buffer-and-window)))))
-          maybe-cleanup
-          (lambda (buffer string)
-            (remove-hook 'compilation-finish-functions maybe-cleanup)
-            (when (and
-                   (string-match-p "compilation" (buffer-name buffer))
-                   (string-match-p "finished" string)
-                   (not
-                    (with-current-buffer buffer
-                      (search-forward "warning" nil t))))
-              (run-with-timer 0.5 nil cleanup buffer))))
-    (add-hook 'compilation-finish-functions maybe-cleanup)
-    (call-interactively 'recompile)))
+(defun ct-compilation-finish-function (buffer string)
+  "Close the compilation buffer when successful."
+  (when (and
+         (string-match-p "compilation" (buffer-name buffer))
+         (string-match-p "finished" string)
+         (not
+          (with-current-buffer buffer
+            (search-forward "warning" nil t))))
+    (run-with-timer 0.5 nil #'delete-window (get-buffer-window buffer t))))
+
+(add-hook 'compilation-finish-functions 'ct-compilation-finish-function)
 
 ;;; Debugging
 
-(defun codetech-gud-mode-hook ()
+(defun ct-gud-mode-hook ()
   (local-set-key (kbd "<f5>") 'gud-step)
   (local-set-key (kbd "<f6>") 'gud-next)
   (local-set-key (kbd "<f7>") 'gud-finish))
 
-(add-hook 'gud-mode-hook 'codetech-gud-mode-hook)
+(add-hook 'gud-mode-hook 'ct-gud-mode-hook)
 
 (global-set-key (kbd "M-b") 'gud-break)
-(global-set-key (kbd "M-S-b") 'gud-remove)
-(global-set-key (kbd "M-p") 'gud-print)
-(global-set-key (kbd "M-i") 'gud-pstar)
-
-;;; Java Debugging
-
-(defun codetech-jdb-command ()
-  "Call some debugger command."
-  (interactive)
-  (let* ((commands '("locals"
-                     "cont"
-                     "help"
-                     "exit"
-                     "run"))
-         (command (ido-completing-read
-                   "Command: " commands nil t)))
-    (when command
-      (gud-call command))))
-
-(defun codetech-jdb-mode-hook ()
-  (local-set-key (kbd "<f8>") 'codetech-jdb-command))
-
-(add-hook 'jdb-mode-hook 'codetech-jdb-mode-hook)
+(global-set-key (kbd "M-n") 'gud-remove)
 
 ;;; Java
 
@@ -160,30 +133,30 @@
 ;; The Makefile must include these lines:
 
 ;;   .PHONY: check-syntax
-
+;;
 ;;   check-syntax:
 ;;           javac -Xlint $(CHK_SOURCES)
 
-(defun codetech-java-flymake-init ()
+(defun ct-java-flymake-init ()
   "Initialize flymake arguments."
   (list "javac" (list (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-with-folder-structure))))
 
 (add-to-list
  'flymake-allowed-file-name-masks
- '("\\.java\\'" codetech-java-flymake-init flymake-simple-cleanup))
+ '("\\.java\\'" ct-java-flymake-init flymake-simple-cleanup))
 
-(defun codetech-java-run ()
+(defun ct-java-run ()
   "Run a program."
   (interactive)
   (shell-command "java Main < sample.in"))
 
-(defun codetech-java-run-diff ()
+(defun ct-java-run-diff ()
   "Test that a program outputs the correct result."
   (interactive)
   (shell-command "java Main < sample.in | diff sample.out -"))
 
-(defun codetech-java-debug ()
+(defun ct-java-debug ()
   "Start a Java debugger."
   (interactive)
   (let* (;; Ensure the debugger opens vertically.
@@ -194,7 +167,8 @@
     (select-window window)
     (jdb "jdb Main sample.in")))
 
-(defun codetech-java-mode-hook ()
+(defun ct-java-mode-hook ()
+  ;; Use a smaller offset to potentially make room for the debugger.
   (setq c-basic-offset 2)
   (flymake-mode-on)
   ;; After using `M-x compile` once (probably to customize the
@@ -202,9 +176,9 @@
   ;; The basic flow is to recompile, and if there are no errors, run
   ;; and diff.  If the program does not produce expected output,
   ;; debug.  Repeat until completion.
-  (local-set-key (kbd "<f5>") 'codetech-recompile)
-  (local-set-key (kbd "<f6>") 'codetech-java-run)
-  (local-set-key (kbd "<f7>") 'codetech-java-run-diff)
-  (local-set-key (kbd "<f8>") 'codetech-java-debug))
+  (local-set-key (kbd "<f5>") 'recompile)
+  (local-set-key (kbd "<f6>") 'ct-java-run)
+  (local-set-key (kbd "<f7>") 'ct-java-run-diff)
+  (local-set-key (kbd "<f8>") 'ct-java-debug))
 
-(add-hook 'java-mode-hook 'codetech-java-mode-hook)
+(add-hook 'java-mode-hook 'ct-java-mode-hook)
